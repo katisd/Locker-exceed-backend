302d08b4a9eca40b47837fc9434874fbfc122b97
Stepz <nine.chaitavat@gmail.com>
2023-02-07 16:25:16 +0700

    update put_checkout

--- app/routers/put_checkout.py
+++ app/routers/put_checkout.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, HTTPException, Body
+from typing import Union, Optional
+from pydantic import BaseModel
+from datetime import date, datetime
+from config.database import mongo_connection
+from math import ceil
 
 
 router = APIRouter(
@@ -6,3 +11,41 @@ router = APIRouter(
     tags=["checkout"],
     responses={404: {"description": "Not found"}},
 )
+
+class Locker(BaseModel):
+    locker_id: int
+    available: bool
+    userId: Optional[str]
+    timeIn: Optional[datetime]
+    timeout: Optional[datetime]
+    package: Optional[str]
+
+@router.get("/{userId}/{locker_id}")                    #คิดค่าใช้จ่ายอย่างเดียว
+def check_out_price(userId: str,locker_id: int):
+    data = mongo_connection["Locker"].find_one({"userId": userId,"locker_id": locker_id},{"_id":False})
+    moreexcess = 0
+    price = 0
+    if not data:
+        raise HTTPException(400, detail="No UserId or LockerId in database")
+    moreexcess = datetime.now().timestamp() - dict(data)["timeout"].timestamp()
+    if moreexcess > 0:
+        price += ceil(abs(moreexcess)/600)*20
+    return price
+
+@router.put("/{userId}/{money}")            #คิดค่าใช้จ่ายและเงินทอนโดยแสดงค่าเงินทอนออกมา สุดท้ายถ้าเงินที่จ่ายเพียงพอจะ update status ใน database
+def check_out_price(userId: str,money: int):
+    data = mongo_connection["Locker"].find_one({"userId": userId,"locker_id": locker_id},{"_id":False})
+    moreexcess = 0
+    price = 0
+    change = 0
+    if not data:
+        raise HTTPException(400, detail="No UserId or LockerId in database")
+    moreexcess = datetime.now().timestamp() - dict(data)["timeout"].timestamp()
+    if moreexcess > 0:
+        price += ceil(abs(moreexcess)/600)*20
+    change = money - price
+    if change < 0:
+        raise HTTPException(400, detail="Insufficient fund")
+    mongo_connection["Locker"].update_one({"userId": userId},{"$set": {"available": True}})
+    mongo_connection["Locker"].update_one({"userId": userId},{"$unset": {"userId": "", "timeIn": "", "timeout": "", "package": ""}})
+    return change
